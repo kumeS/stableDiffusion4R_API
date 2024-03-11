f52d5790bf44e0542e97a13e8042b22fd1785687
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-03-11 20:59:58 +0900

    change index.ts

--- frontend/src/app/index.ts
+++ frontend/src/app/index.ts
@@ -1,7 +1,7 @@
-export { default as Header } from '@/app/_components/Header/index';
-export { default as Top } from '@/app/_components/Top/index';
-export { default as Container } from '@/app/_components/Container/index';
-export { default as About } from '@/app/_components/About/index';
-export { default as GenerateImg } from '@/app/_components/GenerateImg/index';
-export { default as Loading } from '@/app/_components/Loading/index';
-export { default as ModelButton } from '@/app/_components/ModelButton/index';
+export { default as Header } from '@/app/_components/Header';
+export { default as Top } from '@/app/_components/Top';
+export { default as Container } from '@/app/_components/Container';
+export { default as About } from '@/app/_components/About';
+export { default as GenerateImg } from '@/app/_components/GenerateImg';
+export { default as Loading } from '@/app/_components/Loading';
+export { default as ModelButton } from '@/app/_components/ModelButton';
