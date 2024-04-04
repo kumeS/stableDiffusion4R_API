125a063323c8d2b58f74d69aa01f00084cd29761
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-04-04 11:52:05 +0900

    add content of gen_dotenv.cmd

---
+++ gen_dotenv.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+rem 参考サイト: https://qiita.com/shin1rok/items/efb5052ef5fb8138c26d
+rem 参考サイト: https://qiita.com/plcherrim/items/8edf3d3d33a0ae86cb5c
+rem 参考サイト: https://rainbow-engine.com/batch-folderfile-existcheck/
+rem 参考サイト: https://setips.net/bat/bat-exist/
+rem 参考サイト: https://note.alhinc.jp/n/n828e5d7a417f
+rem 参考サイト: https://qiita.com/tera1707/items/e8c5cacac28b2cd7598f
+
+if not exist %~dp0.env (
+    type nul > ./.env
+    echo NEXT_PUBLIC_API_URL=http://localhost:8787>> ./.env
+) else (
+    echo .\.env is already exits.
+)
+
+if not exist %~dp0frontend\.env (
+    type nul > ./frontend/.env
+    echo NEXT_PUBLIC_API_URL=http://localhost:8787>> ./frontend/.env
+) else (
+    echo frontend\.env is already exits.
+)
