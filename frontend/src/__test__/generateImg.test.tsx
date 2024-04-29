9aae81e184749032e6e492d910c0b41e7099c099
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-04-30 02:19:43 +0900

    pre ver. 0.0.2

--- frontend/src/__test__/generateImg.test.tsx
+++ frontend/src/__test__/generateImg.test.tsx
@@ -4,7 +4,7 @@ import { modelType } from '@/constants/api';
 import userEvent from '@testing-library/user-event';
 
 //*Web APIのURLを書く
-const setCorrectUrl = 'http://127.0.0.1:8787';
+const setCorrectUrl = 'http://localhost:8000';
 
 describe('GenerateImg Component Test', () => {
     it('Render Component Test', () => {
